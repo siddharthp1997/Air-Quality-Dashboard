21a3c666d574199dee622a4c8642a9d417ee3ebf
siddharthp1997 <67403303+siddharthp1997@users.noreply.github.com>
2024-06-18 01:49:56 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -15,10 +15,11 @@ API_KEY = os.getenv('API_KEY')
 # MongoDB connection settings
 #MONGO_DB = os.getenv('MONGO_DB')
 #MONGO_COLLECTION = os.getenv('MONGO_COLLECTION')
-MONGO_DB="air_quality"
-MONGO_COLLECTION="city_data"
+MONGO_DB = str(os.getenv('MONGO_DB'))
+MONGO_COLLECTION = str(os.getenv('MONGO_COLLECTION'))
+#MONGO_DB="air_quality"
+#MONGO_COLLECTION="city_data"
 ATLAS_URI = os.getenv('ATLAS_URI')
-print(API_KEY)
 
 # List of major cities in the USA
 cities = [
